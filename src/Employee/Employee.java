67a3376d8c510061e17f2449495709d5023a2afb
AvinashpIntelora <avinash.p@intelora.co.in>
2022-03-08 12:49:50 +0530

    updated:calculate employee parttime wages

--- src/Employee/Employee.java
+++ src/Employee/Employee.java
@@ -3,13 +3,18 @@ package Employee;
 public class Employee
 {
     int PRESENT=1;
+    int PARTTIME=2;
     int WAGE_PRE_HOUR=20;
     int FULL_DAY_HOUR=8;
+    int PART_DAY_HOUR=4;
 
     public int checkEmployeeIsPresent(){
-        int isPresent=(int)Math.round( Math.random() * 1);
+        int isPresent=(int)Math.round( Math.random() * 2);
         if (isPresent == PRESENT){
             System.out.println("Employee is Present");
+        }
+        else if(isPresent==PARTTIME){
+            System.out.println("Employee is Part time Present");
         }
         else{
             System.out.println("Employee is Absent");
@@ -23,6 +28,10 @@ public class Employee
         if ( isEmployeePresent == PRESENT){
             dailyWage= FULL_DAY_HOUR * WAGE_PRE_HOUR;
             System.out.println("Full day total wage is $"+dailyWage);
+        }
+        else if(isEmployeePresent == PARTTIME){
+            dailyWage= PART_DAY_HOUR * WAGE_PRE_HOUR;
+            System.out.println("Part time day total wage is $"+dailyWage);
         }
         else {
             dailyWage=0;
