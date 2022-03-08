b2a7a25d9d184ef25c9bd6290377a0b76ba20b7d
AvinashpIntelora <avinash.p@intelora.co.in>
2022-03-08 12:15:10 +0530

    added: employee attendance class

---
+++ src/Start.java
@@ -0,0 +1,8 @@
+import Employee.EmployeeAttendance;
+
+public class Start {
+    public static void main(String[] args) {
+        EmployeeAttendance employeeAttendance = new EmployeeAttendance();
+        employeeAttendance.checkEmployeeIsPresent();
+    }
+}
