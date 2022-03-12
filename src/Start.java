3e1262f57be0a206d33bc2bc7f9e5067bd76808c
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-03-12 09:15:44 +0530

    Updated: for stashing

--- src/Start.java
+++ src/Start.java
@@ -3,7 +3,6 @@ import Employee.Employee;
 public class Start {
     public static void main(String[] args) {
         Employee obj1 = new Employee("rajesh");
-
         obj1.calculateDailyWage();
 
     }
