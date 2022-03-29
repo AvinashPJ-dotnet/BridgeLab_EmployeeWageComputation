d3b1e8eebc692e08b8486462477193803bbf880a
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-03-29 22:11:03 +0530

    Refactored: some indentation changes and optimization

--- src/Start.java
+++ src/Start.java
@@ -5,6 +5,22 @@ public class Start {
         Employee obj1 = new Employee("rajesh");
         obj1.calculateDailyWage();
 
+        Employee demartEmployee = new Employee("demart");
+        demartEmployee.WORKING_HOURS_PER_MONTH = 110;
+        demartEmployee.WORKING_DAY_PER_MONTH=22;
+        demartEmployee.FULL_TIME_HOURS=10;
+        demartEmployee.PART_TIME_HOURS=5;
+        demartEmployee.WAGE_PER_HOUR=50;
+        demartEmployee.calculateDailyWage();
+
+        Employee relianceEmployee = new Employee("Reliance");
+        relianceEmployee.WORKING_HOURS_PER_MONTH = 105;
+        relianceEmployee.WORKING_DAY_PER_MONTH=18;
+        relianceEmployee.FULL_TIME_HOURS=7;
+        relianceEmployee.PART_TIME_HOURS=3;
+        relianceEmployee.WAGE_PER_HOUR=35;
+        relianceEmployee.calculateDailyWage();
+
     }
 
 }
